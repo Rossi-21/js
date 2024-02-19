27c9832af776b8f75556d147d67bbb99f8ecedf0
Rossi21 <rossi21@gmail.com>
2024-02-19 08:44:36 -0800

    buy health func

--- script.js
+++ script.js
@@ -22,6 +22,25 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+const weapons = [
+    {
+        name: 'stick',
+        power: 5,
+    },
+    {
+    name: 'dagger',
+    power: 30,
+    },
+    {
+        name: 'claw hammer',
+        power: 50,
+    },
+    {
+        name: 'sword',
+        power: 100,
+    }
+];
+
 const locations = [
     {
         name: "town square",
@@ -69,11 +88,29 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    if(gold >= 10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else{
+        text.innerText = "You do not have enough gold to buy health."
+    }
+    
 }
 
 function buyWeapon(){
-
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      }else{
+            text.innerText = "You do not have enough gold to buy a weapon."
+        }
 }
 
 function fightSlime(){
